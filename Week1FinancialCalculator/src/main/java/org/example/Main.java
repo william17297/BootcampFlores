d297a34dbfd8f0de903b85f1f418071ea96dcdc8
william17297 <84396763+william17297@users.noreply.github.com>
2025-04-11 11:46:55 -0500

    Added monthly payment equation

--- Week1FinancialCalculator/src/main/java/org/example/Main.java
+++ Week1FinancialCalculator/src/main/java/org/example/Main.java
@@ -1,7 +1,30 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter your loan amount:");
+    double principal = scanner.nextDouble(); //This is P (Loan amount)
+
+        System.out.println("Enter Interest Rate:");
+    //Used to calculate r
+    double annualInterestRate = scanner.nextDouble();
+
+        System.out.println("Enter loan Length:");
+    //Used to calculate n
+    double loanLength = scanner.nextDouble();
+
+
+    double perMonthInterestRate = (annualInterestRate/12)/100; //This is r
+
+    //Used to calculate M
+    double totalMonthlyPayment = loanLength * 12; //This is n
+
+    double monthlyPayment = principal*(Math.abs(perMonthInterestRate*Math.pow(1+perMonthInterestRate , totalMonthlyPayment)) / Math.abs(Math.pow(1 + perMonthInterestRate , totalMonthlyPayment)-1));
+        System.out.println(monthlyPayment);
     }
 }
