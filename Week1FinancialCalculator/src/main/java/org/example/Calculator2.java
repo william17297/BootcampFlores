aeedc2cbdb60d01f20f7c4b6e8e7ef3a3749fa24
william17297 <84396763+william17297@users.noreply.github.com>
2025-04-11 12:25:23 -0500

    added the calculator that determines future value of cds

--- Week1FinancialCalculator/src/main/java/org/example/Calculator2.java
+++ Week1FinancialCalculator/src/main/java/org/example/Calculator2.java
@@ -1,4 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Calculator2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter initial deposit:");
+        double initialDeposit = scanner.nextDouble(); // This is P
+
+        System.out.println("Enter interest rate:");
+        double yearlyInterestRate = scanner.nextDouble(); //This is r
+
+        System.out.println("Enter number of years deposit has earned interest for:");
+        double yearsInvested = scanner.nextDouble(); // This is t
+
+        double futureValue = initialDeposit * Math.pow(1 + ((yearlyInterestRate/365)/100) , 365 * yearsInvested);
+        System.out.println("You  would have earned $"+ futureValue + " in interest");
+    }
 }
