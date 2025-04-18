a1ac65b651810f24c4f1714b69a0afc43f1b021a
william17297 <84396763+william17297@users.noreply.github.com>
2025-04-18 11:17:17 -0500

    Added the array

---
+++ NeighboorhoodLibrary/src/main/java/org/example/Book.java
@@ -0,0 +1,59 @@
+package org.example;
+
+public class Book {
+    private int id;
+    private String isbn;
+    private String title;
+    private boolean isCheckedOut;
+    private String CheckedOutTo;
+
+    public Book(int id, String isbn, String title, boolean isCheckedOut, String checkedOutTo) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.isCheckedOut = isCheckedOut;
+        CheckedOutTo = checkedOutTo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+
+    public String getCheckedOutTo() {
+        return CheckedOutTo;
+    }
+
+    public void setCheckedOutTo(String checkedOutTo) {
+        CheckedOutTo = checkedOutTo;
+    }
+
+    public 
+}
---
+++ NeighboorhoodLibrary/src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Book[] inventory =new Book[20];
+
+        inventory[0] = new Book(100, "1011234567", "Goosebumps", true, "William");
+        inventory[1] = new Book(101, "1011234568", "Mystery Manor", false, "Agatha");
+        inventory[2] = new Book(102, "1011234569", "The Lost Planet", true, "Clark");
+        inventory[3] = new Book(103, "1011234570", "Under the Sea", true, "Marina");
+        inventory[4] = new Book(104, "1011234571", "Time Traveler", false, "Doc Brown");
+        inventory[5] = new Book(105, "1011234572", "Galaxy Quest", true, "Zara");
+        inventory[6] = new Book(106, "1011234573", "Haunted Hills", false, "Eli");
+        inventory[7] = new Book(107, "1011234574", "Desert Storm", true, "Sahara");
+        inventory[8] = new Book(108, "1011234575", "Jungle Book 2", true, "Rudyard");
+        inventory[9] = new Book(109, "1011234576", "Island Escape", false, "Robinson");
+        inventory[10] = new Book(110, "1011234577", "Dragon’s Fire", true, "Tolkien");
+        inventory[11] = new Book(111, "1011234578", "Winter is Coming", false, "George");
+        inventory[12] = new Book(112, "1011234579", "Cyber Age", true, "Neo");
+        inventory[13] = new Book(113, "1011234580", "Parallel", true, "Quinn");
+        inventory[14] = new Book(114, "1011234581", "Midnight Hour", false, "Stella");
+        inventory[15] = new Book(115, "1011234582", "Ocean’s Deep", true, "Nemo");
+        inventory[16] = new Book(116, "1011234583", "Alien Contact", true, "Sigourney");
+        inventory[17] = new Book(117, "1011234584", "Code Breaker", false, "Alan");
+        inventory[18] = new Book(118, "1011234585", "The Last Stand", true, "Max");
+        inventory[19] = new Book(119, "1011234586", "Crimson Sky", false, "Scarlett");
+    }
+}
